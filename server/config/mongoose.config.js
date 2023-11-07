e3ef73a4fef911d42ce36938e2cc55e6e8762f62
Jasleen Kaur <jasleenkaur2099@gmail.com>
2023-11-06 20:37:09 -0800

    Update mongoose.config.js

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -7,3 +7,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/reviews', {
     .then(() => console.log('Established a connection to the database'))
     .catch(err => console.log('Something went wrong when connecting to the database ', err));
 
+    hi
+

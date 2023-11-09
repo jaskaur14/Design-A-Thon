89baf585eb2c704aff1deed1aa185adc3c795154
Jasleen Kaur <jasleenkaur2099@gmail.com>
2023-11-08 22:07:49 -0800

    added files + mostly done
    added create form, edit form, view form and added content to each; fixed model

---
+++ client/src/components/ViewSubmission.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+const OneDesign = () => {
+
+    const {id} = useParams()
+    const navigate = useNavigate()
+
+    const [design, setDesign] = useState({
+        image: '',
+        commentary: ''
+    })
+    console.log(id)
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/designs/${id}`)
+        .then(res => {
+            console.log(res)
+            setDesigns(res.data.design)
+            console.log(Array.isArray(res.data.designs)) 
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }, [])
+
+    return (
+        <div>
+            <h1 style={{fontFamily: 'cursive'}}>{design.name}</h1>
+            <img src="" alt="" />
+            <label htmlFor="">Upload Design</label>
+            <input type="file" accept='image/jpeg, image/png, image/jpg' />
+            <p>Commentary: {design.commentary}</p>
+        </div>
+    )
+}
+export default OneDesign
--- server/model/design.model.js
+++ server/model/design.model.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 const DesignSchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: [true, "name is required"],
+        minLength: [3, "name must be at least 3 characters long"]
+    },
     image: {
-        type: Image,
+        type: String,
         required: [true, 'image is required']
     },
 
